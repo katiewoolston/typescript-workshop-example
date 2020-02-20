9a3b216ba8efc02e4379dbf452d2bfaf704e64b1
Katie Woolston <katiewoolston@geniesolutions.com.au>
2020-02-21 09:31:26 +1000

    remove 'hasPets' mapping as well

--- scripts/helpers/mapCsvImport.ts
+++ scripts/helpers/mapCsvImport.ts
@@ -1,13 +1,11 @@
 import { State } from './types';
 
-export const mapValues = ({ header, value }: { header: string; value: string }): State|number|boolean|string => {
+export const mapValues = ({ header, value }: { header: string; value: string }): State|number|string => {
   switch(header) {
     case 'stateOfResidence':
       return State[value.toUpperCase() as keyof typeof State];  // enumerable
     case 'age':
       return parseInt(value); // number
-    case 'hasPets':
-      return value.toUpperCase() === 'TRUE'; // boolean
     default:
       return value; // string
   }
